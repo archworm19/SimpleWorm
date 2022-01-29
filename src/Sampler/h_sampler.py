efaa86248516db9b0d427077f4284642632e526a
Zachary Cecere <ztcecere@Zacharys-MacBook-Pro.local>
2022-01-29 13:59:58 -0500

    ready to test anml sampler

--- src/Sampler/h_sampler.py
+++ src/Sampler/h_sampler.py
@@ -5,17 +5,7 @@
     --> return shape = N x M x Twin
     Twin = timewindow
 
-    # TODO: better design
-    # Wut's the design?
-    # > Builder
-    # > > Generate sets from hierarchical data
-    # > > > Sets = indices into data numpy array
-    # > Sampler
-    # > > one sampler for each set
-    # > > houses data and indices into array
-    # > > keeps track of exhausted
 """
-from turtle import shape
 from typing import List
 import numpy as np
 import numpy.random as npr
@@ -207,6 +197,65 @@ class SSFactoryEvenSplit:
                 SmallSampler(full_dat, full_id_dat, np.hstack(test_wins), self.twindow_size)]
 
 
+# TODO: factory for animal splitting
+# much simpler
+class ANMLFactory:
+
+    def __init__(self,
+                 data: List[np.ndarray],
+                 ident_dat: List[np.ndarray],
+                 dat_assign: np.ndarray,
+                 twindow_size: int,
+                 tr_prob = 0.5):
+        """[summary]
+
+        Args:
+            data (List[np.ndarray]): [description]
+            ident_dat (List[np.ndarray]): [description]
+            dat_assign (np.ndarray): which arrays in data
+                are available for sampling
+            twindow_size (int): [description]
+            tr_prob (float, optional): [description]. Defaults to 0.5.
+        """
+        self.data = data
+        self.dat_assign = dat_assign
+        self.ident_dat = ident_dat
+        self.twindow_size = twindow_size
+        self.tr_prob = tr_prob
+        assert(len(self.dat_assign) == len(self.data)), "num anml mismatch"
+
+    def generate_split(self, rand_seed: int):
+        """Generate 50/50 split across animals
+        using provided random seed
+
+        Args:
+            rand_seed (int): random seed for rng
+        """
+        gen = npr.default_rng(rand_seed)
+        # sample at top level --> will need to apply dat_assign later
+        train_anmls = gen.random(len(self.data)) < self.tr_prob
+        # train and test windows ~ can use all overlapping now!
+        offset = 0
+        train_wins, test_wins = [], []
+        for i in range(len(self.data)):
+            # skip unavailable
+            if not self.dat_assign[i]:
+                continue
+            # NOTE: overlapping allowed in this case
+            win = offset + np.arange(0, len(self.data[i] - self.twindow_size))
+            if train_anmls[i]:
+                train_wins.append(win)
+            else:
+                test_wins.append(win)
+            offset += len(win)
+        full_dat = np.vstack([d for d in self.data])
+        full_id_dat = np.vstack([idn for idn in self.ident_dat])
+        return [SmallSampler(full_dat, full_id_dat, np.hstack(train_wins), self.twindow_size),
+                SmallSampler(full_dat, full_id_dat, np.hstack(test_wins), self.twindow_size)]
+
+
+
+
 def _get_max_dat_len(dat: List[np.ndarray]):
     """Helper function to find maximum data length
     across arrays
@@ -228,7 +277,46 @@ def _assert_dim_match(dat: List[np.ndarray]):
         for d in dat[1:]:
             vi = np.array(np.shape(d)[1:])
             assert(np.sum(v0 != vi) < 1), "dim match assertion failure"
-          
+
+
+# TODO: anml factory
+def build_anml_factory(dat: List[np.ndarray],
+                        twindow_size: int,
+                        rand_seed: int = 42):
+    """animal factory
+    break up train-test by animals
+    Assumes: each array in dat is for different animal
+    Assumes: 2/3 train - test split (makes sense assuming 
+        cross-validation in train)
+
+    Args:
+        dat (List[np.ndarray]): animal data
+        twindow_size (int): sampling twindow size 
+            used by sampler
+        rand_seed (int, optional): Defaults to 42.
+    """
+    # get max data length for length normalization:
+    max_dlen = _get_max_dat_len(dat)
+
+    gen = npr.default_rng(rand_seed)
+    train_anmls = gen.random(len(dat)) < 0.667
+
+    # TODO: build identity data:
+    ident_dat = []
+    for i, d in enumerate(dat):
+        ids = i * np.ones((len(d)))
+        tz = np.arange(len(d)) / max_dlen
+
+    train_factory = ANMLFactory(dat,
+                                ident_dat,
+                                train_anmls,
+                                twindow_size)
+    test_factory = ANMLFactory(dat,
+                                ident_dat,
+                                np.logical_not(train_anmls),
+                                twindow_size)
+    return train_factory, test_factory
+
 
 def build_small_factory(dat: List[np.ndarray],
                         twindow_size: int,
